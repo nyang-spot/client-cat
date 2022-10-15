8e6c4d3f03b2a6ff6d16814ac7ec61406cf176e5
MINJE-98 <jmj012100@gmail.com>
2022-10-16 02:10:50 +0900

    feat: rank 관련 스타일

---
+++ src/components/Ranking/Rank.style.ts
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const RankList = styled.li`
+  display: flex;
+  align-items: center;
+  padding-bottom: 5px;
+  padding-top: 5px;
+`;
+
+export const RankLargeImage = styled.div`
+  background-color: black;
+  border-radius: 100%;
+  width: 100px;
+  height: 100px;
+`;
+
+export const LargeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
+
+export const RankContainer = styled.div`
+  padding-right: 20px;
+  padding-left: 20px;
+`;
+
+export const RankImage = styled.div`
+  background-color: black;
+  margin-right: 5px;
+  border-radius: 100%;
+  width: 50px;
+  height: 50px;
+`;
+
+export const HeartContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  margin-left: auto;
+  align-items: center;
+  flex-direction: column;
+`;
