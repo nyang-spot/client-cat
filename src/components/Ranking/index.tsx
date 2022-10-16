6fbb86301453d01bbcb137b06a57748ad5d6d7b4
MINJE-98 <jmj012100@gmail.com>
2022-10-16 22:22:38 +0900

    fix: 기존 컴포넌트 분리 후 적용

--- src/components/Ranking/index.tsx
+++ src/components/Ranking/index.tsx
@@ -1,43 +1,35 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import Heart from '@components/Ranking/Heart';
-import {
-  LargeContainer,
-  RankLargeImage,
-  RankList,
-  RankContainer,
-  RankImage,
-  HeartContainer,
-} from '@components/Ranking/Rank.style';
-import { css } from '@emotion/react';
+import PopularRankSection from '@components/Ranking/PopularRankSection';
+import { RankTitle, RankLocationHighlight } from '@components/Ranking/Rank.style';
+import TopRankSection from '@components/Ranking/TopRankSection';
 import data from './catData.json';
 
 const Ranking = () => {
   const cats = data;
   return (
-    <div>
-      <h1>냥 파이트</h1>
-      <LargeContainer>
-        <RankLargeImage />
-        <h1>vs</h1>
-        <RankLargeImage />
-      </LargeContainer>
-      <RankContainer>
-        {cats.map(cat => (
-          <RankList css={css} key={cat.id}>
-            <RankImage />
-            {cat.description}
-            <HeartContainer>
-              <Heart
-                style={{
-                  marginLeft: 'auto',
-                }}
-              />
-              {cat.likes}
-            </HeartContainer>
-          </RankList>
-        ))}
-      </RankContainer>
+    <div
+      style={{
+        padding: 30,
+      }}
+    >
+      <RankTitle>
+        <RankLocationHighlight>강남구</RankLocationHighlight> 짱 고양이
+      </RankTitle>
+      <div
+        style={{
+          marginTop: 50,
+        }}
+      >
+        <TopRankSection cats={cats} />
+      </div>
+      <div
+        style={{
+          marginTop: 50,
+        }}
+      >
+        <PopularRankSection cats={cats} />
+      </div>
     </div>
   );
 };
