71fd3df45435159f342fddbc7743a7500c22e9b0
MINJE-98 <jmj012100@gmail.com>
2022-10-16 22:22:44 +0900

    fix: 기존 스타일 분리 후 적용

--- src/components/Ranking/Rank.style.ts
+++ src/components/Ranking/Rank.style.ts
@@ -1,44 +1,20 @@
 import styled from '@emotion/styled';
 
-export const RankList = styled.li`
+export const RankLocationHighlight = styled.span`
+  color: #e8b273;
+`;
+export const RankTitle = styled.h1`
+  font-size: 26px;
+  font-weight: 500;
+`;
+export const RankSubTitle = styled.h2`
+  font-size: 16px;
+  font-weight: 500;
+  color: #4b4a47;
+`;
+
+export const RankTitleContainer = styled.div`
   display: flex;
   align-items: center;
-  padding-bottom: 5px;
-  padding-top: 5px;
-`;
-
-export const RankLargeImage = styled.div`
-  background-color: black;
-  border-radius: 100%;
-  width: 100px;
-  height: 100px;
-`;
-
-export const LargeContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: 30px;
-  padding-bottom: 30px;
-`;
-
-export const RankContainer = styled.div`
-  padding-right: 20px;
-  padding-left: 20px;
-`;
-
-export const RankImage = styled.div`
-  background-color: black;
-  margin-right: 5px;
-  border-radius: 100%;
-  width: 50px;
-  height: 50px;
-`;
-
-export const HeartContainer = styled.div`
-  display: flex;
-  font-size: 12px;
-  margin-left: auto;
-  align-items: center;
-  flex-direction: column;
+  margin-bottom: 20px;
 `;
