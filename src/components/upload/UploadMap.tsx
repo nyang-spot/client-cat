684b20c493f3e0bef04e4e2431c1ec270693346e
이시형 <tlgud585@gmail.com>
2022-10-17 15:09:04 +0900

    fix: 위치 아이콘, catData interface 삭제

--- src/components/upload/UploadMap.tsx
+++ src/components/upload/UploadMap.tsx
@@ -1,39 +1,25 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { Container } from './UploadMap.style'
-import { logo, cat3, locationImg } from '../../assets'
-
-
-interface CatData {
-  imageUrl: File | null;
-  description: string;
-  position: {
-    latitude: number;
-    longitude: number;
-  };
-  address: string;
-}
+import React, { useRef, useEffect, useState } from 'react';
+import { Container } from './UploadMap.style';
+import { logo, cat3, locationImg } from '../../assets';
+import { PostData } from '@models/postData';
 
 interface Props {
-  setForm: React.Dispatch<React.SetStateAction<CatData>>;
+  setForm: React.Dispatch<React.SetStateAction<PostData>>;
   addressValid: boolean;
   address: string;
 }
 
-const UploadMap = ({ 
-  setForm,
-  addressValid,
-  address
-}: Props) => {
+const UploadMap = ({ setForm, addressValid, address }: Props) => {
   const defaultLocation = {
     latitude: 37.498095,
     longitude: 127.02761,
-  }
+  };
 
-  const [clickedMap, setClickedMap] = useState<boolean>(false)
+  const [clickedMap, setClickedMap] = useState<boolean>(false);
   const [locationLoading, setLocationLoading] = useState<boolean>(false);
 
   const mapRef = useRef<HTMLElement | null | any>(null);
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null);
 
   const handleMap = () => {
     const { y, x } = mapRef.current.center;
@@ -41,86 +27,91 @@ const UploadMap = ({
     naver.maps.Service.reverseGeocode(
       {
         coords: new naver.maps.LatLng(y, x),
-        orders: [
-          naver.maps.Service.OrderType.ADDR,
-          naver.maps.Service.OrderType.ROAD_ADDR
-        ].join(',')
-      }, 
-      function(status, response) {
+        orders: [naver.maps.Service.OrderType.ADDR, naver.maps.Service.OrderType.ROAD_ADDR].join(','),
+      },
+      function (status, response) {
         if (status !== naver.maps.Service.Status.OK) {
           return alert('Something wrong!');
         }
         address = response.v2.address.jibunAddress;
 
-        setForm((pre)=> {return {
-          ...pre,
-          address,
-          position: {
-            latitude: y,
-            longitude: x
-          }
-        }});
+        setForm(pre => {
+          return {
+            ...pre,
+            address,
+            position: {
+              latitude: y,
+              longitude: x,
+            },
+          };
+        });
       }
     );
-  }
+  };
 
   const handleMapClicked = () => {
-    setClickedMap(true)
-  }
+    setClickedMap(true);
+  };
 
-  useEffect(()=>{
-    const handleMapLeave = (e:MouseEvent) => {
-      if(ref.current && !ref.current.contains(e.target as Node)) {
-        handleMap()
+  useEffect(() => {
+    const handleMapLeave = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handleMap();
       }
-    }
+    };
     clickedMap && document.addEventListener('mouseup', handleMapLeave);
     return () => document.removeEventListener('mouseup', handleMapLeave);
-  })
+  });
 
   const handleLocationButton = () => {
     setLocationLoading(true);
-    
+
     const map = mapRef.current;
-    
+
     function onSuccessGeolocation(position: GeolocationPosition) {
       const location = new naver.maps.LatLng(position.coords.latitude, position.coords.longitude);
       map.setCenter(location);
       setLocationLoading(false);
       handleMap();
     }
-  
+
     function onErrorGeolocation() {
       setLocationLoading(false);
     }
-  
+
     if (window.navigator.geolocation) {
       window.navigator.geolocation.getCurrentPosition(onSuccessGeolocation, onErrorGeolocation);
     }
-  }
+  };
 
   useEffect(() => {
-    mapRef.current =  new naver.maps.Map('map', {
+    mapRef.current = new naver.maps.Map('map', {
       center: new naver.maps.LatLng(defaultLocation.latitude, defaultLocation.longitude),
       zoomControl: false,
     });
-
   }, [mapRef]);
 
   return (
     <Container>
-        <div id='map' ref={ref} onMouseUp={handleMap} onTouchEnd={handleMap}  onMouseDown={handleMapClicked} onTouchStart={handleMapClicked}>
-          <div className='marker'>
-            <img  src={logo}/>
-          </div>
+      <div
+        id='map'
+        ref={ref}
+        onMouseUp={handleMap}
+        onTouchEnd={handleMap}
+        onMouseDown={handleMapClicked}
+        onTouchStart={handleMapClicked}
+      >
+        <div className='marker'>
+          <img src={logo} />
         </div>
-        <div className='address-area'>
-          <div>{address}</div>
-          <img src={locationLoading ? cat3 : locationImg} onClick={handleLocationButton}/>
-        </div>
-        {addressValid ? '' : <div className='warning'>&#8251; 내 지역의 고양이만 등록할 수 있어요</div>}
+      </div>
+      <div className='address-area'>
+        <div>{address}</div>
+        {/* <img src={locationLoading ? cat3 : locationImg} onClick={handleLocationButton} /> */}
+      </div>
+      {addressValid ? '' : <div className='warning'>&#8251; 내 지역의 고양이만 등록할 수 있어요</div>}
     </Container>
-  )
-}
+  );
+};
 
-export default UploadMap
+export default UploadMap;
