279b606a854e57c73e087c4d4a9afbba443a13ec
MINJE-98 <jmj012100@gmail.com>
2022-10-17 15:00:01 +0900

    feat: getCats 함수 추가

--- src/apis/main.ts
+++ src/apis/main.ts
@@ -1,9 +1,9 @@
+import CatsResponse from '@models/catResponse';
 import http from './http';
 
 export const getMarkers = () => {
   return http.get('/cats');
 };
-
 
 export const getCatDetail = (id: number) => {
   return http.get(`/cats/${id}`);
@@ -11,4 +11,9 @@ export const getCatDetail = (id: number) => {
 
 export const postLike = (id: number) => {
   return http.post('/like', { id });
+};
+
+export const getCats = async () => {
+  const { data } = await http.get<CatsResponse[]>('/cats');
+  return data;
 };
