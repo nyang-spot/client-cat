26ca1ea61035fb024ec6e41c95491099d73618ec
MINJE-98 <jmj012100@gmail.com>
2022-10-22 16:03:17 +0900

    chore: init twin.marco

---
+++ twin.d.ts
@@ -0,0 +1,18 @@
+import "twin.macro";
+import { css as cssImport } from "@emotion/react";
+import { CSSInterpolation } from "@emotion/serialize";
+import styledImport from "@emotion/styled";
+
+declare module "twin.macro" {
+  // The styled and css imports
+  const styled: typeof styledImport;
+  const css: typeof cssImport;
+}
+
+declare module "react" {
+  // The tw and css prop
+  interface DOMAttributes {
+    tw?: string;
+    css?: CSSInterpolation;
+  }
+}
