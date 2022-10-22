3592d14d074ea0b4ca84007454d4cb36a4142573
MINJE-98 <jmj012100@gmail.com>
2022-10-22 17:21:08 +0900

    fix: 주석 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 const path = require('path');
-
+/** NOTICE: DocGenPluginDeprecationWarning이 뜹니다.
+ * 왜뜨나요?
+ * StoryBook 스토이북이 아직까지 TypeScript 4.8 버전을 지원하지 않는 것으로 확인됐습니다.
+ * 참고
+ * https://github.com/hipstersmoothie/react-docgen-typescript-plugin/issues/69
+ */
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
