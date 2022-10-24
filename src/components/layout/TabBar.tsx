91b7fba1981e94c7f6b17746510d2106cd51f741
HomieKim <ghals0708@gmail.com>
2022-10-24 16:37:11 +0900

    fix: 탭바 아이콘 색상 변경

--- src/components/layout/TabBar.tsx
+++ src/components/layout/TabBar.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { CustomLink, LayoutTabBar, TabBarName } from './Layout.style';
 import Icon from '@components/icon';
+import tw from 'twin.macro';
 
 const TabBar = () => {
   const TabItem = [
     {
       path: '/main',
-      icon: <Icon type='MAP' fill='black' />,
+      icon: <Icon type='MAP' fill='#4B4A47' />,
       text: '메인',
     },
     {
       path: '/ranking',
-      icon: <Icon type='RANK' fill='black' />,
+      icon: <Icon type='RANK' fill='#4B4A47' />,
       text: '짱 고양이',
     },
     {
       path: '/upload',
-      icon: <Icon type='UPLOAD' fill='black' />,
+      icon: <Icon type='UPLOAD' fill='#4B4A47' />,
       text: '업로드',
     },
     {
       path: '/my-page',
-      icon: <Icon type='PERSON' fill='black' />,
+      icon: <Icon type='PERSON' fill='#4B4A47' />,
       text: '집사',
     },
   ];
